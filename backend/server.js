24402fa7d39efd291254e7c9cdf8bf9d8c2b2589
Rahul Saini <102969842+rahulsaini7284@users.noreply.github.com>
2023-11-21 12:50:23 +0530

    server file updation done

--- backend/server.js
+++ backend/server.js
@@ -23,19 +23,21 @@ app.use("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-const __dirname = path.resolve();
+const __dirname = path.dirname("");
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build/")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-} else {
-  app.use("/", (req, res) => {
-    res.send("API is Runnning....");
-  });
-}
+app.use(express.static(path.join(__dirname, "../frontend/build/")));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
 app.use(notFound);
 app.use(errorHandler);
 
